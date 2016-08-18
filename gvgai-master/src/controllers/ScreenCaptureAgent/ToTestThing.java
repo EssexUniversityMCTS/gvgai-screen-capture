6738726de5a4bb0f04e4707ecdd5b5c4fe41084a
kkunan <kkunan@essex.ac.uk>
2016-08-19 00:18:42 +0100

    more heat maps

--- gvgai-master/src/controllers/ScreenCaptureAgent/ToTestThing.java
+++ gvgai-master/src/controllers/ScreenCaptureAgent/ToTestThing.java
@@ -24,13 +24,17 @@ public class ToTestThing {
 		
 		String lab1 = "I:\\MasterProj\\Code\\output\\labyrinth_lvl0\\400batch\\5by5\\3by3\\dropout_0.0\\0\\heatMap.txt";
 		String lab2 = "I:\\MasterProj\\Code\\output\\labyrinth2_lvl0\\400batch\\5by5\\3by3\\dropout_0.0\\1\\heatMap.txt";
+		String escape0 = "I:\\MasterProj\\Code\\output\\escape_lvl0\\400batch\\5by5\\3by3\\dropout_0.0\\2\\heatMap.txt";
+		String escape5 = "I:\\MasterProj\\Code\\output\\escape_lvl5\\400batch\\5by5\\3by3\\dropout_0.0\\0\\heatMap.txt";
+		String escape6 = "I:\\MasterProj\\Code\\output\\escape_lvl6\\400batch\\5by5\\3by3\\dropout_0.0\\0\\heatMap.txt";
+		String escape3 = "I:\\MasterProj\\Code\\output\\escape_lvl3\\400batch\\5by5\\3by3\\dropout_0.0\\2\\heatMap.txt";
 		
-		BufferedReader b = new BufferedReader(new FileReader(new File(lab2)));
+		BufferedReader b = new BufferedReader(new FileReader(new File(escape3)));
 		
 		ArrayList<double[]> op = new ArrayList();
 		String st = "";
 		
-		int max = 0;
+		double max = 0;
 		
 		while((st = b.readLine())!=null)
 		{
@@ -39,7 +43,7 @@ public class ToTestThing {
 			
 			for(int i=0;i<line.length;i++)
 			{
-				int parse = Integer.parseInt(tmp[i]);
+				double parse = Double.parseDouble(tmp[i]);
 				line[i] = parse;
 				
 				if(parse>max)
@@ -72,7 +76,7 @@ public class ToTestThing {
 		HeatChart map = new HeatChart(real);
 
 		// Step 2: Customise the chart.
-		map.setTitle("Modified Labyrinth heat map");
+		map.setTitle("Escape level 3 heat map");
 		
 		map.setLowValueColour(Color.white);
 		map.setHighValueColour(Color.BLUE);
@@ -82,7 +86,7 @@ public class ToTestThing {
 		
 	
 		// Step 3: Output the chart to a file.
-		map.saveToFile(new File("labyrinth_heat2.png"));
+		map.saveToFile(new File("escape3_heat.png"));
 	}
 
 }
